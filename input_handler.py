dd8d624309e1b86cf1fe4e2ff4ea0ad1eae5282e
František Kýn <frantisek.kyn@proton.me>
2026-04-05 09:23:13 +0200

    Add more keys

--- input_handler.py
+++ input_handler.py
@@ -2,6 +2,7 @@ from evdev import UInput, ecodes as e
 import time
 
 char_map = {
+    # Alphabet
     'a': e.KEY_A, 'b': e.KEY_B, 'c': e.KEY_C, 'd': e.KEY_D,
     'e': e.KEY_E, 'f': e.KEY_F, 'g': e.KEY_G, 'h': e.KEY_H,
     'i': e.KEY_I, 'j': e.KEY_J, 'k': e.KEY_K, 'l': e.KEY_L,
@@ -9,25 +10,35 @@ char_map = {
     'q': e.KEY_Q, 'r': e.KEY_R, 's': e.KEY_S, 't': e.KEY_T,
     'u': e.KEY_U, 'v': e.KEY_V, 'w': e.KEY_W, 'x': e.KEY_X,
     'y': e.KEY_Y, 'z': e.KEY_Z,
+
+    # Numeric
     '0': e.KEY_0, '1': e.KEY_1, '2': e.KEY_2, '3': e.KEY_3,
     '4': e.KEY_4, '5': e.KEY_5, '6': e.KEY_6, '7': e.KEY_7,
     '8': e.KEY_8, '9': e.KEY_9,
-    'space': e.KEY_SPACE,
-    '.': e.KEY_DOT,
-    ',': e.KEY_COMMA,
-    ';': e.KEY_SEMICOLON,
-    "'": e.KEY_APOSTROPHE,
-    '-': e.KEY_MINUS,
-    '=': e.KEY_EQUAL,
-    '/': e.KEY_SLASH,
-    '\\': e.KEY_BACKSLASH,
-    '[': e.KEY_LEFTBRACE,
-    ']': e.KEY_RIGHTBRACE,
-    'enter': e.KEY_ENTER,
-    'shift': e.KEY_LEFTSHIFT, 'ctrl': e.KEY_LEFTCTRL, 'alt': e.KEY_LEFTALT
+
+    # Symbol
+    '.': e.KEY_DOT, ',': e.KEY_COMMA, ';': e.KEY_SEMICOLON,
+    "'": e.KEY_APOSTROPHE, '-': e.KEY_MINUS, '=': e.KEY_EQUAL,
+    '/': e.KEY_SLASH, '\\': e.KEY_BACKSLASH,
+    '[': e.KEY_LEFTBRACE, ']': e.KEY_RIGHTBRACE,
+
+    # Modifier
+    'shift': e.KEY_LEFTSHIFT, 'ctrl': e.KEY_LEFTCTRL, 'alt': e.KEY_LEFTALT, 'meta': e.KEY_LEFTMETA, 'win': e.KEY_LEFTMETA,
+
+    # Function
+    'f1': e.KEY_F1, 'f2': e.KEY_F2, 'f3': e.KEY_F3, 'f4': e.KEY_F4, 'f5': e.KEY_F5, 'f6': e.KEY_F6,
+    'f7': e.KEY_F7, 'f8': e.KEY_F8, 'f9': e.KEY_F9, 'f10': e.KEY_F10, 'f11': e.KEY_F11, 'f12': e.KEY_F12,
+    'f13': e.KEY_F13, 'f14': e.KEY_F14, 'f15': e.KEY_F15, 'f16': e.KEY_F16, 'f17': e.KEY_F17, 'f18': e.KEY_F18,
+    'f19': e.KEY_F19, 'f20': e.KEY_F20, 'f21': e.KEY_F21, 'f22': e.KEY_F22, 'f23': e.KEY_F23, 'f24': e.KEY_F24,
+
+    # Navigation
+    'up': e.KEY_UP, 'down': e.KEY_DOWN, 'left': e.KEY_LEFT, 'right': e.KEY_RIGHT, 'home': e.KEY_HOME,
+    'end': e.KEY_END, 'pageup': e.KEY_PAGEUP, 'pagedown': e.KEY_PAGEDOWN, 'insert': e.KEY_INSERT,
+    'delete': e.KEY_DELETE, 'esc': e.KEY_ESC, 'tab': e.KEY_TAB, 'enter': e.KEY_ENTER, 'backspace': e.KEY_BACKSPACE,
+    'space': e.KEY_SPACE
 }
 
-holdable_keys = ["shift", "ctrl", "alt"]
+holdable_keys = ["shift", "ctrl", "alt", "meta", "win"]
 
 keys_held = []
 
@@ -41,7 +52,6 @@ def handle_input(text):
         ui.write(e.EV_KEY, key, 1)
         ui.syn()
         time.sleep(0.03)
-        print(f"{char}: 1")
 
         if char in holdable_keys:
             keys_held.append(key)
@@ -50,7 +60,6 @@ def handle_input(text):
         ui.write(e.EV_KEY, key, 0)
         ui.syn()
         time.sleep(0.03)
-        print(f"{char}:0")
 
         for hold_key in keys_held:
             ui.write(e.EV_KEY, hold_key, 0)
